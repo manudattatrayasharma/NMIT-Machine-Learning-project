a8b3739b0ae8f4de22eb36010ba804d7f0813bed
manudattatrayasharma <167280425+manudattatrayasharma@users.noreply.github.com>
2024-04-17 15:41:18 +0530

    Update ml_app.py

--- ml_app.py
+++ ml_app.py
@@ -27,7 +27,7 @@ def prediction(Buying,Maint,Doors,Persons,Lug_boots,Safety):
   return result
 
 st.title('car evaluation Classification')
-st.image("""https://www.shutterstock.com/image-photo/yellow-black-29er-mountainbike-thick-600nw-1498702814.jpg""")
+st.image("""https://www.team-bhp.com/sites/default/files/styles/check_extra_large_for_review/public/3_77.jpeg""")
 st.header('Enter the Information of the Car:')
 st.text("vigh = 1 high = 2 med = 3 low = 4")
 Buying = st.number_input('buying:',min_value = 1,max_value = 4,value=1)
