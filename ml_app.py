6b1ad0daaa271c816f62111f6102c1492c034d9c
manudattatrayasharma <167280425+manudattatrayasharma@users.noreply.github.com>
2024-04-17 12:59:50 +0530

    Update ml_app.py

--- ml_app.py
+++ ml_app.py
@@ -2,38 +2,45 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
+
 from sklearn.tree import DecisionTreeClassifier
 
-#loading up the Classification model we created
-
-model = DecisionTreeClassifier(criterion='entropy', max_depth=10, min_samples_leaf=5,
-                       random_state=0)
+#loding up the classification model we created
+model = DecisionTreeClassifier(max_depth=20, min_samples_leaf=5, random_state=0)
 model = joblib.load('finalized_model.joblib')
 
-def prediction(Buying, Maint, Doors, Persons, Lug_boot, safety):
+#caching the model for faster loading
+@st.cache
+
+#Define the prediction function
+
+def prediction(Buying,Maint,Doors,Persons,Lug_boots,Safety):
   if Safety == 'med':
-    Safety = 1
+    safety = 1
   elif Safety == 'high':
-    Safety = 2
+    safety = 2
   elif Safety == 'low':
-    Safety = 3
-  df = pd.DataFrame([Buying, Maint, Doors, Persons, Lug_boot, safety],columns=[Buying, Maint, Doors, Persons, Lug_boot, safety])
-  preduction = model.predict([Buying, Maint, Doors, Persons, Lug_boot, safety])
+    safety = 3
+  df = pd.DataFrame([[Buying,Maint,Doors,Persons,Lug_boots,safety]],
+                    columns=['Buying','Maint','Doors','Persons','Lug_boots','safety'])
+  result=model.predict([[Buying,Maint,Doors,Persons,Lug_boots,safety]])
   return result
-st.title('bike Evaluation Classification')
-st.image("""https://upload.wikimedia.org/wikipedia/commons/0/04/RD-350.jpg""")
-st.header('Enter information of the bike')
-st.text("vhigh = 1 high = 2 med =3 low = 4")
-Buying = st.number_input('Buying','min_values=1','max_values=4',values=1)
-st.text("vhigh = 1 high = 2 med 3 low = 4")
-Maint = st.number_input('maint:', min_value=1, max_value=4, value=1)
-st.text("2-Doors = 1 3-Doors 2 4-Doors = 3 5more = 4")
-Doors=st.number_input('doors:', min_value=1, max_value=4, value=1)
-st.text("2-persons = 1 4-persons = 2 more = 3 ")
-Persons = st.number_input('persons:', min_value=1, max_value=3, value=1)
-st.text("small 1 med 2 big = 3")
-Lug_boot = st.number_input('lug_boot:', min_value=1, max_value=3, value=1)
-Safety = st.radio('safety:', ('med', 'high', 'low'))  
-if st.button('submit_car_Infos'):
-  cal_eval = predict(Buying, Maint, Doors, Persons, Lug_boot, safety)
-  st.success(f'The Evaluation of bike : {cal_eval[0]}')
+
+st.title('car evaluation Classification')
+st.image("""https://www.hindustantimes.com/ht-img/img/2024/04/17/550x309/CRICKET-IND-AFG-T20-62_1709207374217_1713331028912.jpg""")
+st.header('Enter the Information of the Car:')
+st.text("vigh = 1 high = 2 med = 3 low = 4")
+Buying = st.number_input('buying:',min_value = 1,max_value = 4,value=1)
+st.text("vigh = 1 high = 2 med = 3 low = 4")
+Maint = st.number_input('maint:',min_value = 1,max_value = 4,value=1)
+st.text("2-Doors = 1 3-Doors = 2 4-Doors = 3 5more = 4")
+Doors = st.number_input('doors:',min_value = 1,max_value = 4,value=1)
+st.text("2-Persons = 1 4-Persons = 2 more = 3")
+Persons= st.number_input('Persons:',min_value = 1,max_value = 4,value=1)
+st.text("small = 1 med = 2 big = 3")
+Lug_boot= st.number_input('lug_boot:',min_value = 1,max_value = 4,value=1)
+Safety = st.radio('safety:',('med','high','low'))
+
+if st.button('Submit_Cars_Infos'):
+  cal_eval = prediction(Buying,Maint,Doors,Persons,Lug_boot,Safety)
+  st.success(f'The Evaluation of Car : {cal_eval[0]}')
