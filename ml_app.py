d263331551424cfa2648f2383bdda62eeb561cee
manudattatrayasharma <167280425+manudattatrayasharma@users.noreply.github.com>
2024-04-17 12:24:51 +0530

    Update ml_app.py

--- ml_app.py
+++ ml_app.py
@@ -11,15 +11,15 @@ model = DecisionTreeClassifier(criterion='entropy', max_depth=10, min_samples_le
 model = joblib.load('finalized_model.joblib')
 
 def predict(Buying, Maint, Doors, Persons, Lug_boot, safety):
- if Safety == 'med':
-  Safety = 1
- elif Safety == 'high':
-  Safety = 2
- elif Safety == 'low':
-  Safety = 3
- df = pd.DataFrame([Buying, Maint, Doors, Persons, Lug_boot, safety],columns=[Buying, Maint, Doors, Persons, Lug_boot, safety])
-preduction = model.predict([Buying, Maint, Doors, Persons, Lug_boot, safety])
-return prediction
+  if Safety == 'med':
+    Safety = 1
+  elif Safety == 'high':
+    Safety = 2
+  elif Safety == 'low':
+    Safety = 3
+  df = pd.DataFrame([Buying, Maint, Doors, Persons, Lug_boot, safety],columns=[Buying, Maint, Doors, Persons, Lug_boot, safety])
+  preduction = model.predict([Buying, Maint, Doors, Persons, Lug_boot, safety])
+  return prediction
 st.title('bike Evaluation Classification')
 st.image("""https://upload.wikimedia.org/wikipedia/commons/0/04/RD-350.jpg""")
 st.header('Enter information of the bike')
@@ -30,10 +30,10 @@ Maint = st.number_input('maint:', min_value=1, max_value=4, value=1)
 st.text("2-Doors = 1 3-Doors 2 4-Doors = 3 5more = 4")
 Doors=st.number_input('doors:', min_value=1, max_value=4, value=1)
 st.text("2-persons = 1 4-persons = 2 more = 3 ")
-st.number_input('persons:', min_value=1, max_value=3, value=1)
+Persons = st.number_input('persons:', min_value=1, max_value=3, value=1)
 st.text("small 1 med 2 big = 3")
-Lug_boot st.number_input('lug_boot:', min_value=1, max_value=3, value=1)
-Safety st.radio('safety:', ('med', 'high', 'low'))  
+Lug_boot = st.number_input('lug_boot:', min_value=1, max_value=3, value=1)
+Safety = st.radio('safety:', ('med', 'high', 'low'))  
 if st.button('submit_car_Infos'):
-cal_eval = predict(Buying, Maint, Doors, Persons, Lug_boot, safety)
-st.success(f'The Evaluation of bike : {cal_eval[0]}')
+  cal_eval = predict(Buying, Maint, Doors, Persons, Lug_boot, safety)
+  st.success(f'The Evaluation of bike : {cal_eval[0]}')
